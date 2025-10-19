7c5d8be3bf487e13175a2cba18d3ff3c6c1e76f7
Jason Nall <jsonnull@gmail.com>
2025-10-19 15:33:13 -0400

    Setup.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Ultra-minimal direnv dev shell for Yarn/Node";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_22
+              git
+              yarn-berry
+            ];
+          };
+        }
+      );
+    };
+}
